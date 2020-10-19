2376277a5e14069f7d46ba8dc21e9e899327bfc4
Gavin Grant <gavingrant@gmail.com>
2020-10-19 15:12:12 -0700

    feat(functions): add supplemental functions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const LinkedList = require('./linked-list');
 
 const SLL = new LinkedList();
 
-function main() {
+const main = () => {
     SLL.insertFirst('Apollo');
     SLL.insertLast('Boomer');
     SLL.insertLast('Helo');
@@ -18,4 +18,73 @@ function main() {
     return SLL;
 };
 
-console.log(main());
+// Supplemental functions for a linked list
+
+const display = list => {
+    let currNode = list.head;
+    let result = [];
+
+    while (currNode !== null) {
+        result.push(currNode.value);
+        currNode = currNode.next;
+    }
+    
+    return result;
+};
+
+const size = list => {
+    let currNode = list.head;
+    let counter = 0;
+
+    while (currNode !== null) {
+       counter++;
+       currNode = currNode.next;
+    }
+
+    return 'List size: ' + counter;
+};
+
+const isEmpty = list => {
+    if (list.head === null) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const findPrevious = (list, item) => {
+    let currNode = list.head;
+    let prevNode = list.head;
+    
+    while ((currNode !== null) && (currNode.value !== item)) {
+        prevNode = currNode;
+        currNode = currNode.next;
+    }
+
+    if (currNode === null) {
+        return 'Previous not found';
+    } else {
+        return prevNode.value;
+    }
+};
+
+const findLast = list => {
+    let currNode = list.head;
+
+    if (currNode === null) {
+        return 'List doesn\'t exist'
+    }
+
+    while (currNode.next !== null) {
+        currNode = currNode.next;
+    }
+
+    return currNode.value;
+};
+
+main();
+console.log(display(SLL));
+console.log(size(SLL));
+console.log(isEmpty(SLL));
+console.log(findPrevious(SLL, 'Athena'));
+console.log(findLast(SLL));
